be612fb4c029d897c59dce324c7f2f63b771e45e
idc-666 <idc@100ui.cn>
2025-06-09 13:09:45 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,48 @@
+const CACHE_NAME = "v1"
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "https://framerusercontent.com/images/mWvzExg6MygDc3XL3JG6kUXkM.png",
+  "https://framerusercontent.com/images/rizRdHVJA7UrXDlGUEY6AgPuT0.png",
+]
+
 self.addEventListener("install", (e) => {
+  console.log("[SW] Installing...")
   e.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "https://framerusercontent.com/images/mWvzExg6MygDc3XL3JG6kUXkM.png",
-        "https://framerusercontent.com/images/rizRdHVJA7UrXDlGUEY6AgPuT0.png"
-      ])
+    caches.open(CACHE_NAME).then((cache) => { 
+      return cache.addAll(urlsToCache)
     })
-  );
-});
+  )
+})
+
+self.addEventListener("activate", (e) => {
+  console.log("[SW] Activating...")
+  const cacheWhitelist = [CACHE_NAME]
+  e.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key)
+          }
+        })
+      )
+    )
+  )
+})
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+      return (
+        response ||
+        fetch(e.request).catch(() => {
+          if (e.request.mode === "navigate") {
+            return caches.match("/")
+          }
+        })
+      )
     })
-  );
-});
+  )
+})
